3cdc754c7a3d401904d9a0f6ebd9792345ebc69a
Moritz Profitlich <moritz@profitlich.ch>
2022-12-27 12:17:14 +0100

    Aufgeräumt und an aktuellen Stand COR und CYA angepasst

--- config/general.php
+++ config/general.php
@@ -11,27 +11,36 @@
 use craft\config\GeneralConfig;
 use craft\helpers\App;
 
+// Jede Umgebung isXxx wird true oder false
+// Abfrage mit ->allowAdminChanges($isXxx) oder invers ->allowAdminChanges(!$isDev)
+$isDev = App::env('CRAFT_ENVIRONMENT') === 'dev';
+$isStaging = App::env('CRAFT_ENVIRONMENT') === 'staging';
+$isProduction = App::env('CRAFT_ENVIRONMENT') === 'production';
+
+// https://nystudio107.com/blog/fluent-multi-environment-config-for-craft-cms-4
 return GeneralConfig::create()
     // Set the default week start day for date pickers (0 = Sunday, 1 = Monday, etc.)
     ->defaultWeekStartDay(1)
     // Prevent generated URLs from including "index.php"
     ->omitScriptNameInUrls()
     // Enable Dev Mode (see https://craftcms.com/guides/what-dev-mode-does)
-    ->devMode(App::env('DEV_MODE') ?? false)
+    ->devMode($isDev)
     // Allow administrative changes
-    ->allowAdminChanges(App::env('ALLOW_ADMIN_CHANGES') ?? false)
+    ->allowAdminChanges($isDev)
     // Disallow robots
-    ->disallowRobots(App::env('DISALLOW_ROBOTS') ?? false)
+    ->disallowRobots(!$isProduction)
 
+    // Slugs ohne Umlaute
+    ->limitAutoSlugsToAscii(true)
+    // Loginadresse des Redaktionssystems
+    ->cpTrigger('redaktion')
+    
+    // Aliases _nicht_ mit getenv() aus Sicherheitsgründen
+    // https://craftcms.stackexchange.com/questions/36986/use-of-appenv-compared-to-getenv-since-craft-3-4-18
     ->aliases([
+        '@web' => craft\helpers\App::env('BASE_URL'),
         '@webroot' => dirname(__DIR__) . '/web',
         '@assetBaseUrl' => craft\helpers\App::env('ASSETS_BASE_URL'),
         '@assetBasePath' => craft\helpers\App::env('ASSETS_BASE_PATH'),
     ])
-
-    // Slugs ohne Umlaute
-    ->limitAutoSlugsToAscii(true)
-    
-    // Loginadresse des Redaktionssystems
-    ->cpTrigger('redaktion')
 ;
