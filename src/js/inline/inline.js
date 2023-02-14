0fa1667c04233c74782b5b3ac2323484112c34ea
Filipa Fernandes Moreira <f.fernandesmoreira@hotmail.com>
2023-02-14 12:06:47 +0100

    100vh als Variabel für IOS

--- src/js/inline/inline.js
+++ src/js/inline/inline.js
@@ -35,3 +35,19 @@ for(var i = (emlArray.length - 1); i >= 0; i--) {
     obj.setAttribute('href', emlAddress);
     obj.innerHTML = emlText;
 }
+
+// 100vh Problem
+// https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
+
+function vh() {
+    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+    let vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+vh();
+
+// We listen to the resize event
+window.addEventListener('resize', () => {
+    vh();
+});
