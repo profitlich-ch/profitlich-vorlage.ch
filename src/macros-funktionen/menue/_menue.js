98a3a91dcd267f6f39cfc982a633f58ce740a973
filimoreira <f.fernandesmoreira@hotmail.com>
2023-06-19 17:36:29 +0200

    menue.js mit OOP

--- src/macros-funktionen/menue/_menue.js
+++ src/macros-funktionen/menue/_menue.js
@@ -1,104 +1,108 @@
-var menueAktiv = false;
-var seite = document.getElementById('seite');
-var hamburger = document.getElementById('hamburger')
-
-// Alle Elemente finden, die tatsächlich sticky sind
-var stickyElemente = [];
-function stickyElementeFinden() {
-    var stickyElementeAlle = document.querySelectorAll('[data-sticky]');
-    if (stickyElementeAlle) {
-        for (let i = 0; i < stickyElementeAlle.length; i++) {
-            if (window.getComputedStyle(stickyElementeAlle[i]).position == 'sticky') {
-                stickyElemente.push(stickyElementeAlle[i]);
-            }
-        }
+class StickyMenu {
+    constructor() {
+      this.menuActive = false;
+      this.page = document.getElementById('seite');
+      this.hamburger = document.getElementById('hamburger');
+      this.stickyElements = [];
+      this.stickyElementsArray = [];
+      this.y = 0;
+  
+      this.init();
     }
-}
-stickyElementeFinden();
-
-var y = 0;
-const stickyElementeArray = [];
-function menueToggle() {
-    if (menueAktiv == false) {
-        if (typeof stickyElemente != 'undefined') {
-            for (let i = 0; i < stickyElemente.length; i++) {
-                // Bestehende top Position in Array speichern, um sie beim Deaktivieren des Menüs wiederherzustellen
-                var stickyElementStyles = window.getComputedStyle(stickyElemente[i]);
-                stickyElementeArray[i] = [];
-                stickyElementeArray[i].top = stickyElementStyles.top;
-
-                // Element statt sticky neu fixed positionieren
-                // Dafür muss der top-Wert auf den tatsächlichen Abstand zum viewport gesetzt werden
-                var stickyElementTop = stickyElemente[i].getBoundingClientRect().top;
-                var stickyElementWidth = stickyElemente[i].getBoundingClientRect().width;
-                stickyElemente[i].style.top = stickyElementTop + 'px';
-                stickyElemente[i].style.width = stickyElementWidth + 'px';
-                stickyElemente[i].style.position = 'fixed';
-            }
+  
+    init() {
+      this.stickyElementsFind();
+      this.hamburger.addEventListener('click', event => {
+        event.preventDefault();
+        event.stopPropagation();
+        this.toggleMenu();
+      });
+  
+      const menu = document.getElementById('menue__menue');
+      menu.addEventListener('click', event => {
+        if (this.menuActive) {
+          const { target } = event;
+          if (target.matches('.menue__link')) {
+            this.toggleMenu();
+          }
         }
-        menueAktiv = true;
-        // Klick ausserhalb des Menüs
-        document.body.addEventListener('click', menueBodyClick);
-        // Hover ausserhalb des Menüs
-        seite.addEventListener('mouseenter', menueSeiteHover);
-        seite.addEventListener('mouseleave', menueSeiteHover);
-
-        scrollbar = window.innerWidth - document.documentElement.clientWidth;
-        y = window.scrollY;
-        document.body.setAttribute('data-menue-aktiv', menueAktiv);
-        var marginOriginal = parseFloat(window.getComputedStyle(hamburger).marginRight);
-        hamburger.style.marginRight = marginOriginal + scrollbar + 'px';
+      });
+    }
+  
+    stickyElementsFind() {
+      const stickyElementsAll = document.querySelectorAll('[data-sticky]');
+      Array.from(stickyElementsAll).forEach(element => {
+        if (window.getComputedStyle(element).position === 'sticky') {
+          this.stickyElements.push(element);
+        }
+      });
+    }
+  
+    toggleMenu() {
+      if (!this.menuActive) {
+        if (this.stickyElements.length > 0) {
+          this.stickyElements.forEach((stickyElement, index) => {
+            const stickyElementStyles = window.getComputedStyle(stickyElement);
+            this.stickyElementsArray[index] = {
+              top: stickyElementStyles.top,
+              width: stickyElementStyles.width
+            };
+  
+            const stickyElementTop = stickyElement.getBoundingClientRect().top;
+            const stickyElementWidth = stickyElement.getBoundingClientRect().width;
+            stickyElement.style.top = stickyElementTop + 'px';
+            stickyElement.style.width = stickyElementWidth + 'px';
+            stickyElement.style.position = 'fixed';
+          });
+        }
+  
+        this.menuActive = true;
+        document.body.addEventListener('click', this.handleBodyClick.bind(this));
+        this.page.addEventListener('mouseenter', this.handlePageHover.bind(this));
+        this.page.addEventListener('mouseleave', this.handlePageHover.bind(this));
+  
+        const scrollbar = window.innerWidth - document.documentElement.clientWidth;
+        this.y = window.scrollY;
+        document.body.setAttribute('data-menue-aktiv', this.menuActive);
+        const marginOriginal = parseFloat(window.getComputedStyle(this.hamburger).marginRight);
+        this.hamburger.style.marginRight = marginOriginal + scrollbar + 'px';
         document.body.style.paddingRight = scrollbar + 'px';
-        document.body.style.top = -y + 'px';
-    } else {
-        if (typeof stickyElemente != 'undefined') {
-            for (let i = 0; i < stickyElemente.length; i++) {
-                stickyElemente[i].style.top = stickyElementeArray[i].top;
-                stickyElemente[i].style.position = 'sticky';
-            }
+        document.body.style.top = -this.y + 'px';
+      } else {
+        if (this.stickyElements.length > 0) {
+          this.stickyElements.forEach((stickyElement, index) => {
+            stickyElement.style.top = this.stickyElementsArray[index].top;
+            stickyElement.style.position = 'sticky';
+          });
         }
-        menueAktiv = false;
-        document.body.removeEventListener('click', menueBodyClick);
-        seite.removeEventListener('mouseenter', menueSeiteHover);
-        seite.removeEventListener('mouseleave', menueSeiteHover);
-        document.body.setAttribute('data-menue-aktiv', menueAktiv);
-        menueSeiteHover(false);
-        hamburger.style.marginRight = '';
+  
+        this.menuActive = false;
+        document.body.removeEventListener('click', this.handleBodyClick.bind(this));
+        this.page.removeEventListener('mouseenter', this.handlePageHover.bind(this));
+        this.page.removeEventListener('mouseleave', this.handlePageHover.bind(this));
+        document.body.setAttribute('data-menue-aktiv', this.menuActive);
+        this.handlePageHover(false);
+        this.hamburger.style.marginRight = '';
         document.body.style.paddingRight = '';
         document.body.style.top = '';
-        window.scrollTo(0, y);
+        window.scrollTo(0, this.y);
+      }
     }
-}
-
-function menueBodyClick(event) {
-    if (menueAktiv == true) {
-        if (!event.target.closest('.menue__menue')) {
-            menueToggle();
-        }
+  
+    handleBodyClick(event) {
+      if (this.menuActive && !event.target.closest('.menue__menue')) {
+        this.toggleMenu();
+      }
     }
-}
-
-function menueSeiteHover(event) {
-    if (event.type == 'mouseenter') {
+  
+    handlePageHover(event) {
+      if (event && event.type === 'mouseenter') {
         document.body.setAttribute('data-seite-hover', 'true');
-    } else {
+      } else {
         document.body.setAttribute('data-seite-hover', 'false');
+      }
     }
-}
-
-// Hamburgermenü aktivieren
-document.getElementById('hamburger').addEventListener('click', event => {
-    event.preventDefault();
-    event.stopPropagation();
-    menueToggle();
-});
-
-var menue = document.getElementById('menue__menue');
-menue.addEventListener('click', event => {
-    if (menueAktiv == true) {
-        const { target } = event;
-        if (target.matches('.menue__link')) {
-            menueToggle();
-        }
-    }
-});
+  }
+  
+  new StickyMenu();
+  
