baef76baf7809ccf4bddaace0ab1dec4c7e3a00c
Moritz Profitlich <m.profitlich@profitlich.ch>
2024-11-01 19:40:24 +0100

    Bugfixes zu Umstellung auf @use

--- config/custom.php
+++ config/custom.php
@@ -16,7 +16,7 @@
         
         // production environment
         'production'  => array(
-            'staticAssetsVersion' => 1730410362,
+            'staticAssetsVersion' => 1730486370,
         ),
 
         // staging environment
